5cc61e9ea80bacf1064133ddcda1672a6650ab20
Iktaniyalol <ilonellochka7@gmail.com>
2021-04-21 22:33:15 +1000

    Выкладываю первые изменения

---
+++ Vector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic; 
+
+public static class Vector
+{
+    private static LinkedList<Point> points = new LinkedList<Point>();
+
+    public static Point getFirstP()
+    {
+        Console.Write(points.First.Value);
+        return points.First.Value;
+    }
+}
